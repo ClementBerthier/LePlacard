0d2a751c0dfeaac5feabcdb64c6f6432ad4d7d3d
ClementBerthier <berthierclementpro@gmail.com>
2023-05-24 21:08:06 +0200

    add log counter for each elements imported

--- Back/script/seeding.js
+++ Back/script/seeding.js
@@ -17,11 +17,14 @@ async function deleteData() {
     await client.query(
         'TRUNCATE "user", games, boxs, armies, decors, figurines RESTART IDENTITY CASCADE;'
     );
+    console.log("Data successfully deleted");
 }
 
 // 1. register user
 
 async function importUser() {
+    let counter = 0;
+
     for (const user of users) {
         const sqlQuery = `
         INSERT INTO public.user
@@ -33,12 +36,17 @@ async function importUser() {
         const result = await client.query(sqlQuery);
 
         user.id = result.rows[0].id;
+
+        counter++;
     }
+    console.log(`nb of user imported : ${counter}`);
 }
 
 // 2. register games
 
 async function importGames() {
+    let counter = 0;
+
     for (const game of games) {
         const sqlQuery = `
         INSERT INTO public.games
@@ -50,12 +58,16 @@ async function importGames() {
         const result = await client.query(sqlQuery);
 
         game.id = result.rows[0].id;
+        counter++;
     }
+    console.log(`nb of game imported : ${counter}`);
 }
 
 // 3. register boxs
 
 async function importBoxs() {
+    let counter = 0;
+
     for (const box of boxs) {
         const game = games.find((game) => game.name === box.game_name);
 
@@ -69,12 +81,17 @@ async function importBoxs() {
         const result = await client.query(sqlQuery);
 
         box.id = result.rows[0].id;
+        counter++;
     }
+
+    console.log(`nb of box imported : ${counter}`);
 }
 
 // 4. register armies
 
 async function importArmies() {
+    let counter = 0;
+
     for (const army of armies) {
         const box = boxs.find((box) => box.name === army.box_name);
         const game = games.find((game) => game.name === army.game_name);
@@ -88,12 +105,17 @@ async function importArmies() {
 
         const result = await client.query(sqlQuery);
         army.id = result.rows[0].id;
+        counter++;
     }
+
+    console.log(`nb of army imported : ${counter}`);
 }
 
 // 5. register decors
 
 async function importDecors() {
+    let counter = 0;
+
     for (const decor of decors) {
         const box = boxs.find((box) => box.name === decor.box_name);
         const user = users.find((user) => user.identifiant === decor.user_name);
@@ -105,12 +127,18 @@ async function importDecors() {
         ('${decor.name}', '${decor.picture_path}', '${decor.purchase}', '${decor.cleanMount}', '${decor.undercoat}', '${decor.paint}', '${decor.plinth}', '${decor.varnish}', '${decor.box_name}', '${decor.user_name}', '${box.id}', '${user.id}')`;
 
         await client.query(sqlQuery);
+
+        counter++;
     }
+
+    console.log(`nb of decor imported : ${counter}`);
 }
 
 // 6. register figurines
 
 async function importFigurines() {
+    let counter = 0;
+
     for (const figurine of figurines) {
         const army = armies.find((army) => army.name === figurine.army_name);
         const user = users.find(
@@ -123,7 +151,11 @@ async function importFigurines() {
         ('${figurine.name}', '${figurine.picture_path}', '${figurine.purchase}', '${figurine.cleanMount}', '${figurine.undercoat}', '${figurine.paint}', '${figurine.plinth}', '${figurine.varnish}', '${figurine.army_name}', '${figurine.user_name}', '${army.id}', '${user.id}')`;
 
         await client.query(sqlQuery);
+
+        counter++;
     }
+
+    console.log(`nb of figurine imported : ${counter}`);
 }
 
 // import Data in datadase.
