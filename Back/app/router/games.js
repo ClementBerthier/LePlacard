a7b5b18e83d0e349f0553cf0eaa2dcf6c0c5c9f3
ClementBerthier <berthierclementpro@gmail.com>
2023-10-27 16:25:43 +0200

    router game

--- Back/app/router/games.js
+++ Back/app/router/games.js
@@ -1,1 +1,15 @@
 import { express } from "express";
+import { gamesController } from "../controllers";
+
+const router = express.Router();
+
+router.get("/", gamesController.getAllGames);
+router.get("/:id", gamesController.getOneGame);
+
+router.post("/", gamesController.addGame);
+
+router.patch("/:id", gamesController.updateGame);
+
+router.delete("/:id", gamesController.deleteGame);
+
+modules.exports = router;
