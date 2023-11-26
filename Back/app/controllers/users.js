94d6a711db6d470cec30f729653c9ded93d620d3
ClementBerthier <berthierclementpro@gmail.com>
2023-11-26 16:08:51 +0100

    createUser bcrypt ok

--- Back/app/controllers/users.js
+++ Back/app/controllers/users.js
@@ -1,17 +1,25 @@
 const coreController = require("./coreController.js");
 const userModel = require("../models/users.js");
+const bcrypt = require("bcrypt");
 
 const usersController = {
     method: coreController.listMethod(userModel, "user"),
 
     async createUser(req, res) {
+        const salt = 10;
+
         try {
-            
             const user = req.body;
-            const userDB = await userModel.insertUser(user, "user");
+
+            const passwordHash = await bcrypt.hash(user.password_user, salt);
+            user.password_user = passwordHash;
+            console.log(user);
+            const userDB = await userModel.insert(user, "user");
             res.json(userDB);
         } catch (error) {}
     },
+
+    async loginUser(req, res) {},
 };
 
 module.exports = usersController;
