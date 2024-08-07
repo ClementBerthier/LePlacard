7ef34431d3c586de278181117a8b0c57ace5be58
ClementBerthier <berthierclementpro@gmail.com>
2024-08-07 11:51:23 +0200

    header style ok

--- Front/src/components/Header/Header.jsx
+++ Front/src/components/Header/Header.jsx
@@ -1,5 +1,22 @@
 import "./Header.css";
 
 export default function Header() {
-    return <div></div>;
+    return (
+        <div className="Header">
+            <div className="header_links">
+                <a className="link" href="/home">
+                    Accueil
+                </a>
+                <a className="link" href="/search">
+                    Recherche
+                </a>
+                <a className="link" href="/management">
+                    Gestion
+                </a>
+                <button className="logout" href="/logout">
+                    Déconnexion
+                </button>
+            </div>
+        </div>
+    );
 }
