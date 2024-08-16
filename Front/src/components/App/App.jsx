af0e3ab9b53677f489656284110fb613bfe49c28
Clément <berthierclementpro@gmail.com>
2024-08-16 13:12:18 +0200

    request front game and army

--- Front/src/components/App/App.jsx
+++ Front/src/components/App/App.jsx
@@ -7,7 +7,7 @@ export default function App() {
     return (
         <>
             <Routes>
-                <Route path="/login" element={<LoginPage />} />
+                <Route path="/" element={<LoginPage />} />
                 <Route path="/home" element={<Home />} />
             </Routes>
         </>
--- Front/src/components/Home/Home.jsx
+++ Front/src/components/Home/Home.jsx
@@ -1,7 +1,57 @@
+import { useEffect, useState } from "react";
 import Header from "../Header/Header.jsx";
+import api from "../../services/api.js";
 import "./Home.css";
 
+//TODO: regler le probleme de decalage de donnée (donné décalé d'une requette )lors de la recupération des data armies
+
 export default function Home() {
+    const token = localStorage.getItem("token");
+    const [allGames, setAllGames] = useState([]);
+    const [armiesOfGame, setArmiesOfGame] = useState([]);
+
+    async function getAllArmiesByGame(gameName) {
+        try {
+            const response = await api.get("/armies/armiesOfGame", {
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                    nameofgame: gameName,
+                },
+            });
+            const listOfArmy = response.data;
+            console.log("armies of game", armiesOfGame);
+            return setArmiesOfGame(listOfArmy);
+        } catch (error) {
+            console.error(error);
+        }
+    }
+    async function getAllGames() {
+        try {
+            const response = await api.get("/games", {
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                },
+            });
+            setAllGames(response.data);
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+    useEffect(() => {
+        getAllGames();
+    }, []);
+    console.log(allGames);
+
+    const handleChangeGames = (e) => {
+        let gameSelectionned = e.target.value;
+        getAllArmiesByGame(gameSelectionned);
+    };
+
+    const handleChangeArmies = (e) => {
+        let armySelectionned = e.target.value;
+        console.log(armySelectionned);
+    };
     return (
         <>
             <Header />
@@ -9,15 +59,48 @@ export default function Home() {
                 <h1 className="home_title">Le Placard</h1>
                 <div className="game_selector">
                     <h2 className="list_game_title">Liste de vos jeux</h2>
-                    <select name="" id="">
+                    <select
+                        name="allGames"
+                        id="allGames"
+                        onChange={handleChangeGames}
+                    >
                         <option value="Total">Total</option>
+                        {allGames.map((game) => (
+                            <option key={game.id} value={game.game_name}>
+                                {game.game_name}
+                            </option>
+                        ))}
                     </select>
-                    <h2 className="list_army_title">
-                        Selection de l{"'"}armée
-                    </h2>
-                    <select name="" id="">
-                        <option value=""></option>
-                    </select>
+                    <div className="secondary_selector">
+                        <div>
+                            <h2 className="list_army_title">
+                                Selection de l{"'"}armée
+                            </h2>
+                            <select
+                                name="allArmiesinGame"
+                                id="allArmiesinGame"
+                                onChange={handleChangeArmies}
+                            >
+                                <option value="Total">Total</option>
+                                {armiesOfGame.map((army) => (
+                                    <option
+                                        key={army.id}
+                                        value={army.army_name}
+                                    >
+                                        {army.army_name}
+                                    </option>
+                                ))}
+                            </select>
+                        </div>
+                        <div>
+                            <h2 className="list_box_title">
+                                Selection de la boite
+                            </h2>
+                            <select name="" id="">
+                                <option value=""></option>
+                            </select>
+                        </div>
+                    </div>
                 </div>
                 <h2 className="detail_title">Détails</h2>
                 <div className="total_section_container">
